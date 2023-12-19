210fbdc8b568a11b5b459237ea1cf6f71d087c70
Shaun Sweet <shaun@integrate.co>
2023-12-18 23:38:34 -0700

    fix: things

--- release.config.cjs
+++ release.config.cjs
@@ -3,15 +3,12 @@
  */
 module.exports = {
   branches: ['main', 'next'],
+
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
   ],
 }
