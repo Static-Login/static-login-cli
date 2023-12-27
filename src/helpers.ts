95df71e002fb09e358b379991bffc258cf7602e9
Shaun Sweet <shaun@integrate.co>
2023-12-26 19:00:47 -0700

    chore: swap to tar instead of zip

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,3 +4,25 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 
 export const __dirname = path.dirname(__filename);
+
+export const streamToBlob = (
+  stream: NodeJS.ReadableStream,
+  mimeType: string | null
+) => {
+  if (mimeType != null && typeof mimeType !== "string") {
+    throw new Error("Invalid mimetype, expected string.");
+  }
+  return new Promise<Blob>((resolve, reject) => {
+    const chunks: any = [];
+    stream
+      .on("data", (chunk) => chunks.push(chunk))
+      .once("end", () => {
+        const blob =
+          mimeType != null
+            ? new Blob(chunks, { type: mimeType })
+            : new Blob(chunks);
+        resolve(blob);
+      })
+      .once("error", reject);
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
 #!/usr/bin/env node
 import figlet from "figlet";
 import axios from "axios";
-import fs from "node:fs";
-import { __dirname } from "./helpers";
+import { __dirname, streamToBlob } from "./helpers";
 import path from "node:path";
-import AdmZip from "adm-zip";
+import tar from "tar";
 
 console.log(figlet.textSync("Static Login"));
 
 export const main = async () => {
   const formData = new FormData();
-  const directoryToZip = path.join(__dirname, "..", "testDist");
-  const zip = new AdmZip();
-  const outputFile = "site.zip";
-  zip.addLocalFolder(directoryToZip);
-  zip.writeZip(outputFile);
-  const outputPath = path.join(__dirname, "..");
-  console.log(outputPath);
+  const directoryToTar = path.join(__dirname, "..", "testDir");
+  console.log(directoryToTar);
+  const tarStream = tar.create(
+    {
+      C: directoryToTar,
+    },
+    ["."]
+  );
+  const file = await streamToBlob(tarStream, null);
 
-  const file = fs.readFileSync(path.join(outputPath, outputFile));
-
-  formData.append("file", new Blob([file]), outputFile);
+  formData.append("file", new Blob([file]), "tarball.tgz");
 
   await axios.post("http://localhost:3000/api/upload", formData, {
     headers: {
